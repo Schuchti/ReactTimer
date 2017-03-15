29dc19b5c6b2f26dfd33b72ce73da7e754e6800a
oschucht <oschucht@tecracer.de>
2017-03-15 12:10:36 +0100

    Update status to stop when count reaches zero - bugfix

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -38,7 +38,11 @@ var Countdown = React.createClass ({
         var newCount = this.state.count - 1;
         this.setState({
           count: newCount >= 0 ? newCount : 0
-        })
+        });
+
+        if (newCount === 0 ) {
+          this.setState({countdownStatus: 'stopped'});
+        };
       }, 1000);
   },
 
